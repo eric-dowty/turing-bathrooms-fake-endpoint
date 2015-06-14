3752ca3b1061291c87e97e521c79c7d74db8e54d
end3rs-army <eric.m.dowty@gmail.com>
2015-06-14 11:24:44 -0600

    fake endpoint ready for deploy

---
+++ fake-endpoint/spec/controllers/api/v1/bathrooms_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::BathroomsController" do
+
+  # describe "does this work" do
+  #   get :index, format: :json
+  #   status = JSON.parse(response.body, symbolize_names: true)
+
+  #   expect(status[:bathroom1]).to be_within(1).of(0)
+  # end
+
+end
