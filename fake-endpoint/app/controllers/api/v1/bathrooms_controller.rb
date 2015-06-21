3a0b51700fc1958677ea796d313c0b2812fd5c4c
end3rs-army <eric.m.dowty@gmail.com>
2015-06-21 16:17:00 -0600

    added api key for status access

--- fake-endpoint/app/controllers/api/v1/bathrooms_controller.rb
+++ fake-endpoint/app/controllers/api/v1/bathrooms_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::BathroomsController < ApplicationController
   end
 
   def status
-    Bathroom.updates(params["status"])
+    Bathroom.updates(params["status"]) if params["key"] == 'a71cbb3cc730e252d1512258671a6bb7'
     respond_with Bathroom.statuses
   end
 
