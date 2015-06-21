f93b6ba0922b9734ce200dc76b192e70e96a3dc1
end3rs-army <eric.m.dowty@gmail.com>
2015-06-21 15:56:48 -0600

    seed file for bathrooms

--- fake-endpoint/db/seeds.rb
+++ fake-endpoint/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bathroom.create!(description: 'bathroom1', status: 0)
+Bathroom.create!(description: 'bathroom2', status: 0)
+Bathroom.create!(description: 'bathroom3', status: 0)
